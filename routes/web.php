1c2d2e74c40485415412b3cdbcdf20c4286da8ab
Le Truong <letruong07042004@gmail.com>
2025-02-22 23:44:33 +0700

    update db

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Models\Student;
 
+
 Route::get('/', function () {
     return view('welcome');
 });
@@ -17,6 +18,6 @@ Route::get('/students/create', function () {
 });
 
 Route::get('/students/{id}', function($id){
-    $student = Student::findOrFail($id);
+    $student = Student::findOrFail($id); // findOrFail() Tra ve 1 dong du lieu tuong ung voi id, neu khong tim thay se tra ve loi 404
     return view('students.show', ["id" => $id, "student" => $student]);
 });
